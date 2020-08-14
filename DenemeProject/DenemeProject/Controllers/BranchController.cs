690c911ecd04e319ea2a425026f88234be94b28d
ulaspolatli <ulaspolatli@hotmail.com>
2020-08-14 18:02:48 +0300

    Dev1 BranchController

---
+++ DenemeProject/DenemeProject/Controllers/BranchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DenemeProject.Controllers
+{
+    public class BranchController : Controller
+    {
+        // GET: Branch
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Bu controller Dev1 adlı Branch'te (Yedekte) oluşturuldu";
+            return View();
+        }
+    }
+}
